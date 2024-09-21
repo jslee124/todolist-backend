bf0f0103ca2f604337f8c1f58703e81e91a3ded3
jslee <mori71004@gmail.com>
2024-09-21 21:17:47 +0800

    basically finished the project

--- src/main/java/com/example/accessingdatamysql/TodoController.java
+++ src/main/java/com/example/accessingdatamysql/TodoController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @CrossOrigin                             // 前后端不在同一域名下 需要跨域访问
 @Controller
 @RequestMapping(path = "/todo")
@@ -25,6 +27,36 @@ public class TodoController {
         newTodo.setCompleted(isCompleted);
         todoRepository.save(newTodo);
         return "Saved";
+    }
+
+    @PostMapping("/update")
+    public @ResponseBody String updateTodo(
+            @RequestParam Integer id, @RequestParam String name
+    )
+    {
+        Optional<Todo> oldTodo = todoRepository.findById(id);
+        if (oldTodo.isPresent()) {
+            Todo newTodo = new Todo();
+            newTodo.setId(id);
+            newTodo.setName(name);
+            newTodo.setCompleted(oldTodo.get().isCompleted());
+            todoRepository.save(newTodo);
+        } else {
+            return "id is not present";
+        }
+        return "Updated";
+    }
+
+    @PostMapping("/complete")
+    public @ResponseBody String completeTodo(@RequestParam Integer id){
+        if (todoRepository.findById(id).isPresent()) {
+            Todo todo = todoRepository.findById(id).get();
+            todo.setCompleted(!todo.isCompleted());
+            todoRepository.save(todo);
+        } else {
+            return "id is not present";
+        }
+        return "Updated";
     }
 
     // localhost:8080/todo/delete
